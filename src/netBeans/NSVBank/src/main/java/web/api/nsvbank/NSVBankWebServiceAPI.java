8a6d1fa093027c13f552e427d4648895f14f4c37
Navjot Singh Virk <virksaabnavjot@gmail.com>
2016-11-21 16:59:34 +0000

    Added GET request
    Available at: http://localhost:8080/api/bank/

--- src/netBeans/NSVBank/src/main/java/web/api/nsvbank/NSVBankWebServiceAPI.java
+++ src/netBeans/NSVBank/src/main/java/web/api/nsvbank/NSVBankWebServiceAPI.java
@@ -1,12 +1,36 @@
 //Project Github: https://github.com/Virksaabnavjot/NSVBank
 package web.api.nsvbank;
 
+import com.google.gson.Gson;
+import java.util.ArrayList;
+import java.util.List;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+
 /**
  *
  * @author Navjot Singh Virk
  * Date: 21st November 2016
  * Time: 4:02 pm (Dublin,Ireland)
  */
+
+@Path("bank")
+@Produces("application/json")
 public class NSVBankWebServiceAPI {
-    
+    public static final String bankName = "NSV Bank Ireland";
+    @GET
+    @Path("/")
+    @Produces("application/json")
+    public Response test(@Context UriInfo info){
+        List <String> list = new ArrayList<>();
+        String text = "You can use our online banking.Have a Good Day!!!!";
+        list.add(bankName);
+        list.add(text);
+        Gson gson = new Gson();
+        return Response.status(200).entity(gson.toJson(list)).build();
+    }
 }
