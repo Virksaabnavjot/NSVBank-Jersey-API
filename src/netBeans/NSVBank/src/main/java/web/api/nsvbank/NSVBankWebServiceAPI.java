ca25cc972b375c62b3042b5852d81a9ea21ee13d
Navjot Singh Virk <virksaabnavjot@gmail.com>
2016-11-23 21:30:06 +0000

    Written Accounts GET Request
    Written AccountController and TransactionController class and now the
    API supports get requests for accounts
    URL: http://localhost:8080/api/bank/accounts/
    
    The Output results are posted down below in the comments section of the
    commit

--- src/netBeans/NSVBank/src/main/java/web/api/nsvbank/NSVBankWebServiceAPI.java
+++ src/netBeans/NSVBank/src/main/java/web/api/nsvbank/NSVBankWebServiceAPI.java
@@ -10,12 +10,17 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
+import web.api.nsvbank.account.Account;
+import web.api.nsvbank.account.AccountController;
 
 /**
  *
  * @author Navjot Singh Virk
  * Date: 21st November 2016
  * Time: 4:02 pm (Dublin,Ireland)
+ * 
+ * @Modification History
+ * Modified on: 23rd November/2016 by Navjot at 9:02pm
  */
 
 @Path("bank")
@@ -32,5 +37,15 @@ public class NSVBankWebServiceAPI {
         list.add(text);
         Gson gson = new Gson();
         return Response.status(200).entity(gson.toJson(list)).build();
+    }
+    
+    @GET
+    @Path("/accounts")
+    @Produces("application/json")
+    public Response accounts(@Context UriInfo info){
+        AccountController accountController = new AccountController();
+        List <Account> accounts = accountController.getAccounts();
+        Gson gson = new Gson();
+        return Response.status(200).entity(gson.toJson(accounts)).build();
     }
 }
--- src/netBeans/NSVBank/src/main/java/web/api/nsvbank/transaction/TransactionController.java
+++ src/netBeans/NSVBank/src/main/java/web/api/nsvbank/transaction/TransactionController.java
@@ -1,11 +1,28 @@
 package web.api.nsvbank.transaction;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Navjot Singh Virk
  * Date: 21st November 2016
  * Time: 4:02 pm (Dublin,Ireland)
+ * 
+ * @Modification History
+ * Modified on: 23rd November/2016 by Navjot at 9:23pm
  */
 public class TransactionController {
+    List <Transaction> transactions;
     
+    public TransactionController(){
+        transactions = new ArrayList();
+    }
+
+    public List<Transaction> getTransactions() {
+        Transaction transaction = new Transaction();
+        transactions.add(transaction);
+        return transactions;
+    }
+   
 }
