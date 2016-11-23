ee9495a77dfc067354ba6eb79c89350c54d25966
Navjot Singh Virk <virksaabnavjot@gmail.com>
2016-11-23 21:41:24 +0000

    Written Transactions GET Request
    Now API returns a list of transactions
    URL: http://localhost:8080/api/bank/transactions
    
    Result screenshot available in comments section of the commit.

--- src/netBeans/NSVBank/src/main/java/web/api/nsvbank/NSVBankWebServiceAPI.java
+++ src/netBeans/NSVBank/src/main/java/web/api/nsvbank/NSVBankWebServiceAPI.java
@@ -12,6 +12,8 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 import web.api.nsvbank.account.Account;
 import web.api.nsvbank.account.AccountController;
+import web.api.nsvbank.transaction.Transaction;
+import web.api.nsvbank.transaction.TransactionController;
 
 /**
  *
@@ -20,7 +22,7 @@ import web.api.nsvbank.account.AccountController;
  * Time: 4:02 pm (Dublin,Ireland)
  * 
  * @Modification History
- * Modified on: 23rd November/2016 by Navjot at 9:02pm
+ * Modified on: 23rd November/2016 by Navjot at 9:02pm, 9:39pm
  */
 
 @Path("bank")
@@ -48,4 +50,15 @@ public class NSVBankWebServiceAPI {
         Gson gson = new Gson();
         return Response.status(200).entity(gson.toJson(accounts)).build();
     }
+    
+    @GET
+    @Path("/transactions")
+    @Produces("application/json")
+    public Response transactions(@Context UriInfo info){
+        TransactionController transactionController = new TransactionController();
+        List <Transaction> transactions = transactionController.getTransactions();
+        Gson gson = new Gson();
+        return Response.status(200).entity(gson.toJson(transactions)).build();
+    }
+    
 }
